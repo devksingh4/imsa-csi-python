783694f95a14e18e8e8cdeb60b3b0aac286b1860
Dev Singh <dev@singhk.dev>
2020-04-09 15:25:49 -0500

    f

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,4 +1,4 @@
-from functools import reduce # reduce applies specified function to first 2 elements, then to the next element after applying the function to the first 2.
+from functools import reduce
 def main():
     scores = []
     while 1 == 1: 
@@ -6,7 +6,7 @@ def main():
             scores.append(int(input("Enter a score to have the average computed of. If you would not like to enter another score, simply press enter.: ")))
         except ValueError:
             break
-    avg = 0 if len(scores) == 0 else reduce(lambda z, a:z+a, scores) / len(scores) # avoid divide by 0 error if no scores were entered
+    avg = 0 if len(scores) == 0 else reduce(lambda z, a:z+a, scores) / len(scores)
     print("The average score is: ", avg)
 
 main()
