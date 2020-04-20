aaeb38ece10d6bec7ee5be0539f59091ecb42f38
Dev Singh <dev@singhk.dev>
2020-04-20 09:32:25 -0500

    grrrrr squashing bugs

--- einstein/einstein.py
+++ einstein/einstein.py
@@ -3,16 +3,22 @@ def get_input():
     number = 0
     try:
         number = int(input("What is the number you would like to play with?: "))
-    except:  #tries to run some code in the try block, and if the code doesn't work, then run the except block
+    except ValueError as e:  #tries to run some code in the try block, and if the code doesn't work, then run the except block
+        print(e)
         print("Invalid Input")
-        get_input()
-    return list(map(lambda x: int(x), list(str(number)))) # lambda and map allow me to run a function and set the return value to the new value of the list element.
+        number = get_input()
+    try:
+        rval = list(map(lambda x: int(x), list(str(number)))) # lambda and map allow me to run a function and set the return value to the new value of the list element.
+    except ValueError:
+        rval = number
+    return rval 
 def main():
     print("""Welcome to the Albert Einstien program. \nIt is said that Albert Einstein used to take great delight in baffling friends with the puzzle below. \n I guarantee to you that I will turn any 3 digit number that you provide me within the constraints into the number 1089.  \n The number you provide me must: \n - be a 3 digit number \n - have the first and list digits differ by at least 2 \n Lets play! \n""")
     num0 = []
     int_num = 0
     while True:  
         num0 = get_input()  
+        print(num0)
         if (len(num0) == 3 and abs(num0[0] - num0[2]) > 1):  
             break  # breaks out of the while loop to go to the next line.
         else: 
