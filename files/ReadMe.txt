a048e44b2ebcbadac533b01851e5fc36575ccb17
Dev Singh <dev@singhk.dev>
2020-04-23 16:59:25 -0500

    Make it work
    Signed-off-by: Dev Singh <dev@singhk.dev>

--- files/ReadMe.txt
+++ files/ReadMe.txt
@@ -1,16 +1,1 @@
 Hello
-Hi
-Memer
-
-Dev     Thu Apr 23 13:43:11 2020
-
-Dev     Thu Apr 23 14:48:39 2020
-
-Jacob     Thu Apr 23 14:54:43 2020
-
-Your     Thu Apr 23 15:09:26 2020
-
-Dev     Thu Apr 23 15:16:08 2020
-
-Dev     Thu Apr 23 15:17:31 2020
-
--- files/SinghFiles.cpp
+++ files/SinghFiles.cpp
@@ -2,9 +2,11 @@
 #include <iostream> // import the iostream library which allows me to output to the console and recieve input.
 #include <ctime> //allows me to get the current time to write to the file. 
 #include<stdio.h> // allows me to get space seperated input
+#include <vector> //allows for use of dynamically-sized arrays
+#include <typeinfo> // get type of a variable
 using namespace std; // use the standard namespace which contains string, cout, etc. so i dont have to keep typing std::cout
 
-// Prototype all the functions so I don't have to call them in order.
+// Prototype all the neccessary functions so I don't have to call them in order.
 string readFile(string fileName);
 void writeToFile(string name, string date);
 void writeToFile(string method);
@@ -36,26 +38,22 @@ string readFile (string fileName) {
 }
 
 void writeToFile(string name, string date) {
-  // Create and open a text file
-  ofstream f("ReadMe.txt", std::ios_base::app);
-
-  // Write to the file
-  f << name << "     " << date << "\n";
-
-  // Close the file
-  f.close();
+    ofstream f("ReadMe.txt", ios_base::app); // Open the file
+    f << name << "     " << date << endl; // Write to the file
+    f.close(); // close the file
 } 
 
 void writeToFile(string method) {
-  // Create and open a text file
-  ofstream f("Relax.txt", std::ios_base::app);
-
-  // Write to the file
-  f << method << "\n";
-
-  // Close the file
-  f.close();
+    ofstream f("Relax.txt", ios_base::app); // Open the file
+    f << method << endl; // Write to the file
+    f.close(); // close the file
 } 
+void writeToBonus(string file, string method) {
+    ofstream f(file, ios_base::app); // Open the file
+    f << method << endl; // Write to the file
+    f.close(); // close the file
+} 
+
 
 void option2a() {
     int inc = 0;
@@ -76,6 +74,8 @@ void option2a() {
     for (int i = 0; i < 5; i++) {
         writeToFile(inputs[i]);
     }
+    cout << readFile("Relax.txt");
+    prompt();
 }
 
 void option2b() {
@@ -89,16 +89,16 @@ void option2b() {
     getline(cin, hi);
     hi.append("\n");
     writeToFile(hi);
-    cout << "Added!" << "\n";
+    cout << "Added!" << endl;
     cout << readFile("Relax.txt");
     prompt();
 }
 
 void option1 () {
     string fileOuptut = readFile("ReadMe.txt");
-    cout << "Here is what is in the file: " << "\n";
-    cout << fileOuptut << "\n";
-    cout << "Now, I need to get what your name is for the visitor log. Please enter your name: ";
+    cout << "Here is what is in the file: " << endl;
+    cout << fileOuptut << endl;
+    cout << "Now, I need to get what your name is for the visitor log. Please enter your first name: ";
     string userName;
     string dt;
     cin >> userName;
@@ -129,9 +129,63 @@ string option2 () {
     }
     return choice;
 }
-
+string getnum1 () {
+    string num1;
+    cout << "Enter Number 1:" << endl;
+    cin>>num1;
+    try {
+        stoi(num1);
+    } catch(std::invalid_argument e1) {
+        if (num1 != "stop"){
+            cout << "Invalid input" << endl; 
+            num1 = getnum1();
+        }
+    }
+    return num1;
+}
+string getnum2 () {
+    string num1;
+    cout << "Enter Number 2:" << endl;
+    cin>>num1;
+    try {
+        stoi(num1);
+    } catch(std::invalid_argument e1) {
+        if (num1 != "stop"){
+            cout << "Invalid input" << endl; 
+            num1 = getnum2();
+        }
+    }
+    return num1;
+}
 void bonus () {
-    cout << "not implemented" << endl;
+    string filename;
+    cout << "Enter the filename of the file you would like to write to: " << endl;
+    cin >> filename;
+    vector<vector<string>> array;
+    string num1;
+    string num2;
+    vector<string> interims;
+    cout << "At any time, enter 'stop' for both values to exit" << endl;
+    while(num1 != "stop"){
+        interims.clear();
+        num1 = getnum1();
+        num2 = getnum2();
+        if (num1 == "stop" || num2 == "stop") {
+            break;
+        } else {
+            interims.push_back(num1);
+            interims.push_back(num2);
+            array.push_back(interims);
+        }
+    }
+    string output;
+    for (int i = 0; i < array.size(); i++) {
+        string sum = to_string(stoi(array[i][0]) + stoi(array[i][1]));
+        output = array[i][0] + "+" + array[i][1] + "=" + sum + "\n";
+        writeToBonus(filename, output);
+    }
+    cout << readFile(filename);
+    prompt();
 }
 
 string getChoice () {
@@ -164,11 +218,11 @@ void prompt() {
     }
 }
 int main ()  {
-    cout << "Hello and welcome to this program!" << "\n";
-    cout << "You see, I just came to the realization that the CSI assignment descriptions never actually specify that these programs must be done in python." << "\n";
-    cout << "Knowing me, I decided to exploit this loophole" << "\n";
-    cout << "Have fun!" << "\n";
-    cout << "----------------------------" << "\n";
+    cout << "Hello and welcome to this program!" << endl;
+    cout << "You see, I just came to the realization that the CSI assignment descriptions never actually specify that these programs must be done in python." << endl;
+    cout << "Knowing me, I decided to exploit this loophole" << endl;
+    cout << "Have fun!" << endl;
+    cout << "----------------------------" << endl;
     prompt();
 }
 
